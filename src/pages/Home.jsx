062002d772a977c702a266567f9d1a1447488912
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-10-11 17:01:13 -0300

    alterei projeto delevery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                             </div>
                             <div class="carousel-inner">
-                                <div class="carousel-item active">
+                                <div class="carousel-item active sobremesas">
                                     <div className='publi-container'>
                                         <div className='publi-text'>
                                             <h1>Sobremesas Irresistíveis <br />para Adoçar Seu Dia!</h1>
@@ -35,8 +35,16 @@ function Home() {
                                         </figure>
                                     </div>
                                 </div>
-                                <div class="carousel-item">
-                                    <img src="massas.png" class="d-block w-100" alt="..." />
+                                <div class="carousel-item massas">
+                                    <div className='publi-container'>
+                                        <div className='publi-text'>
+                                            <h1>Massas perfeitas para todos os momentos!</h1>
+                                            <p>Se você é fã de uma refeição prática e deliciosa, nossas massas são a escolha ideal!</p>
+                                        </div>
+                                        <figure>
+                                            <img src="massas.png" alt="" />
+                                        </figure>
+                                    </div>
                                 </div>
                                 <div class="carousel-item">
                                     <img src="..." class="d-block w-100" alt="..." />
@@ -51,6 +59,38 @@ function Home() {
                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Next</span>
                             </button>
+                        </div>
+                    </div>
+                </section>
+                <section className='section-dishes'>
+                    <h1>Pratos do dia</h1>
+                    <div className='cards-dishes-container'>
+                        <div class="card">
+                            <img src="https://via.placeholder.com/300x200" alt="Imagem do Prato" class="card-image" />
+                            <div class="card-content">
+                                <h2 class="card-title">Strogonoff de Frango</h2>
+                                <p class="card-description">Delicioso strogonoff de frango com arroz soltinho, batata palha e molho cremoso.</p>
+                                <p class="card-price">R$ 20,00</p>
+                                <a href="#" class="card-button">Peça Agora</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img src="https://via.placeholder.com/300x200" alt="Imagem do Prato" class="card-image" />
+                            <div class="card-content">
+                                <h2 class="card-title">Strogonoff de Frango</h2>
+                                <p class="card-description">Delicioso strogonoff de frango com arroz soltinho, batata palha e molho cremoso.</p>
+                                <p class="card-price">R$ 20,00</p>
+                                <a href="#" class="card-button">Peça Agora</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img src="https://via.placeholder.com/300x200" alt="Imagem do Prato" class="card-image" />
+                            <div class="card-content">
+                                <h2 class="card-title">Strogonoff de Frango</h2>
+                                <p class="card-description">Delicioso strogonoff de frango com arroz soltinho, batata palha e molho cremoso.</p>
+                                <p class="card-price">R$ 20,00</p>
+                                <a href="#" class="card-button">Peça Agora</a>
+                            </div>
                         </div>
                     </div>
                 </section>
