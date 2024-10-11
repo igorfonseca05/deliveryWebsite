34cab50b56601571225bb64a7f067727d1a54c2e
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-10-11 15:48:19 -0300

    alterei projeto delevery

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,40 +8,24 @@ function Modal() {
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Criar Conta</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <form class="form modal-body">
-                            <p class="title">Register </p>
-                            <p class="message">Signup now and get full access to our app. </p>
-                            <div class="flex">
-                                <label>
-                                    <input required="" placeholder="" type="text" class="input" />
-                                    <span>Firstname</span>
-                                </label>
-
-                                <label>
-                                    <input required="" placeholder="" type="text" class="input" />
-                                    <span>Lastname</span>
-                                </label>
+                        <div class="form-box modal-body">
+                            <form class="form">
+                                <span class="title">Cadastrar</span>
+                                <span class="subtitle">Crie uma conta grátis com seu email.</span>
+                                <div class="form-container">
+                                    <input type="text" class="input" placeholder="Nome completo" />
+                                    <input type="email" class="input" placeholder="Email" />
+                                    <input type="password" class="input" placeholder="Senha" />
+                                </div>
+                                <button>Cadastrar</button>
+                            </form>
+                            <div class="form-section">
+                                <p>Já tem uma conta? <a href="">Entrar</a> </p>
                             </div>
-
-                            <label>
-                                <input required="" placeholder="" type="email" class="input" />
-                                <span>Email</span>
-                            </label>
-
-                            <label>
-                                <input required="" placeholder="" type="password" class="input" />
-                                <span>Password</span>
-                            </label>
-                            <label>
-                                <input required="" placeholder="" type="password" class="input" />
-                                <span>Confirm password</span>
-                            </label>
-                            <button class="submit">Submit</button>
-                            <p class="signin">Already have an acount ? <a href="#">Signin</a> </p>
-                        </form>
+                        </div>
                         {/* <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             <button type="button" class="btn btn-primary">Understood</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,49 @@ function Home() {
         <div>
             <header className='bg-body-tertiary'>
                 <ResponsiveMenu />
-                <Modal />
             </header>
+            <main>
+                <section className='hero_section'>
+                    <Modal />
+                    <div className='carrosel-container'>
+                        <div id="carouselExampleIndicators" class="carousel slide">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div className='publi-container'>
+                                        <div className='publi-text'>
+                                            <h1>Sobremesas Irresistíveis <br />para Adoçar Seu Dia!</h1>
+                                            <p>Delicie-se com nossas sobremesas feitas com carinho, perfeitas para finalizar sua refeição com um toque doce e especial.</p>
+                                        </div>
+                                        <figure>
+                                            <img src="https://static.vecteezy.com/system/resources/thumbnails/017/394/920/small_2x/sweet-strawberry-ice-cream-generative-ai-png.png" alt="" />
+                                        </figure>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="massas.png" class="d-block w-100" alt="..." />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="..." class="d-block w-100" alt="..." />
+                                    <h1>Meu nome é gal</h1>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            </main>
 
         </div>
     )
