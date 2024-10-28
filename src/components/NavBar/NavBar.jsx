2a583d68e241226a5fc7768ecc3ad28c0b2f51ef
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-10-28 15:14:28 -0300

    alterei projeto delivery

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { NavLink } from 'react-router-dom'
 
 import "./NavBar.css"
 
 function NavBar() {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    function handleClick() {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <header>
-            <nav>
-                <div>
-                    <div className="logo">
-                        <img src="#" alt="logo" />
-                    </div>
-                    <div className='search_input'>
-                        <input type="text" />
-                        <button>
-                            <span className="material-symbols-outlined">
-                                search
-                            </span>
-                        </button>
+            <nav className='top_navbar'>
+                <span className='material-symbols-outlined' onClick={handleClick}>menu</span>
+                <div className="logo-top-menu">
+                    <img src="logo.svg" alt="logo" />
+                </div>
+                <div className='search_input'>
+                    <input type="text" />
+                    <button>
+                        <span className="material-symbols-outlined">
+                            search
+                        </span>
+                    </button>
+                </div>
+            </nav>
+            <nav className={`lateral_navBar ${isOpen ? 'open' : ''}`}>
+                <div className=''>
+                    <span className='material-symbols-outlined' onClick={handleClick}>menu</span>
+                    <div className="lateral-logo">
+                        <img src="logoIcon.svg" alt="logo" />
                     </div>
                 </div>
+
                 <ul className='li_container'>
                     <NavLink><li className='nav_item'> <span className="material-symbols-outlined menu-icon">home</span> <p>Home</p></li></NavLink>
                     <NavLink><li className='nav_item'> <span className='material-symbols-outlined menu-icon'>lunch_dining</span> <p>Cardápio</p></li></NavLink>
