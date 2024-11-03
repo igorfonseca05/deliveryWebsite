a1cb7f0ffce426ec35072e8dbe2d40c5d247dc36
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-11-03 20:53:47 -0300

    adicionando estilos ao container de pedidos

--- src/components/carrosel/card/Card.jsx
+++ src/components/carrosel/card/Card.jsx
@@ -5,15 +5,15 @@ import './Card.css'
 function Card({ title, subtitle, imgSource }) {
     return (
         <div className='card_container'>
-            <div className='cards_infos_container'>
+            {/* <div className='cards_infos_container'>
                 <p className='Card_carousel_title'>{title}</p>
                 <p className='Card_corousel_subtitle'>{subtitle}</p>
-            </div>
+            </div> */}
             <figure className='figure_container'>
                 {/* <img src={imgSource} alt="Imagem 1" /> */}
                 <img
-                    src="sor5.png"
-                    srcset="sor-320.png 320w, sor-640.png 640w, sor5.png 1024w"
+                    src="banner mobile.png"
+                    srcset="banner mobile.png 320w, banner mobile.png 640w, banner mobile.png 1024w"
                     alt="Descrição da imagem"
                 />
             </figure>
