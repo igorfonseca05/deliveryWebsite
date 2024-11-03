78942dad68b9bc481bef9c0bf9e4237a45ae8ab0
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-11-03 12:27:50 -0300

    adicionando estilos ao container de pedidos

--- src/components/carrosel/card/Card.jsx
+++ src/components/carrosel/card/Card.jsx
@@ -10,7 +10,12 @@ function Card({ title, subtitle, imgSource }) {
                 <p className='Card_corousel_subtitle'>{subtitle}</p>
             </div>
             <figure className='figure_container'>
-                <img srcSet={`${imgSource + '376w'}`} alt="Imagem 1" />
+                {/* <img src={imgSource} alt="Imagem 1" /> */}
+                <img
+                    src="sor5.png"
+                    srcset="sor-320.png 320w, sor-640.png 640w, sor5.png 1024w"
+                    alt="Descrição da imagem"
+                />
             </figure>
         </div>
     )
