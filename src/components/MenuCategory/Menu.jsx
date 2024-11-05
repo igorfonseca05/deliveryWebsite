a2cbc924703557a8f2edbdba18c44b6873f31b17
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-11-05 16:25:39 -0300

    criando produtos container

--- src/components/MenuCategory/Menu.jsx
+++ src/components/MenuCategory/Menu.jsx
@@ -6,17 +6,59 @@ import React from 'react'
 import './Menu.css'
 
 function Menu() {
+
+    function handleColor(e) {
+        const lis = [...document.querySelector('.menu_category_item_container').children]
+
+        lis.forEach(li => {
+            li.classList.remove('ativo')
+        })
+
+        e.target.tagName === 'LI' || e.target.tagName === 'SPAN' ? e.target.classList.add('ativo') : ''
+    }
+
+
     return (
         <div className='Menu_category_container'>
             <ul className='menu_category_item_container'>
-                <li className="menu_category_item">
-                    <span class="material-symbols-outlined">stockpot</span>Todos</li>
-                <li className="menu_category_item">Do dia</li>
-                <li className="menu_category_item">Combos</li>
-                <li className="menu_category_item">Especialidade</li>
-                <li className="menu_category_item">Massas</li>
-                <li className="menu_category_item">Bebidas</li>
-                <li className="menu_category_item">Porções</li>
+                <li className="category_item ativo">
+                    <button>
+                        <span className="material-symbols-outlined">stockpot</span>
+                        Todos
+                    </button>
+                </li>
+                <li className="category_item">
+                    <button>
+                        <span className="material-symbols-outlined">calendar_month</span>Do dia
+                    </button>
+                </li>
+                <li className="category_item">
+                    <button>
+                        <span className="material-symbols-outlined">fastfood</span>
+                        Combos
+                    </button>
+                </li>
+                <li className="category_item">
+                    <button>Especialidade</button>
+                </li>
+                <li className="category_item">
+                    <button>
+                        <span className="material-symbols-outlined">dinner_dining</span>
+                        Massas
+                    </button>
+                </li>
+                <li className="category_item">
+                    <button>
+                        <i className="bi bi-cup-straw"></i>
+                        Bebidas
+                    </button>
+                </li>
+                <li className="category_item">
+                    <button>
+                        <span className="material-symbols-outlined">kebab_dining</span>
+                        Porções
+                    </button>
+                </li>
             </ul>
         </div>
     )
